5c343f6e3e0d3d76ecf924f7bcd74c89c712185a
ThanhNguyen <thanh.nguyen5@sotatek.com>
2023-03-23 14:34:37 +0700

    test zz

--- src/main.ts
+++ src/main.ts
@@ -10,27 +10,27 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule, {
     snapshot: true,
-    // preview: shouldPublishGraph,
+    preview: shouldPublishGraph,
   });
 
-  // if (shouldPublishGraph) {
-  //   await app.init();
+  if (shouldPublishGraph) {
+    await app.init();
 
-  //   const publishOptions = {
-  //     apiKey: process.env.DEVTOOLS_API_KEY,
-  //     repository: process.env.REPOSITORY_NAME,
-  //     owner: process.env.GITHUB_REPOSITORY_OWNER,
-  //     sha: process.env.COMMIT_SHA,
-  //     target: process.env.TARGET_SHA,
-  //     trigger: process.env.GITHUB_BASE_REF ? 'pull' : 'push',
-  //     branch: process.env.BRANCH_NAME,
-  //   } as PublishGraphOptions; // NOTE: this options object will vary depending on the CI/CD provider you're using
-  //   const graphPublisher = new GraphPublisher(app);
-  //   await graphPublisher.publish(publishOptions);
+    const publishOptions = {
+      apiKey: process.env.DEVTOOLS_API_KEY,
+      repository: process.env.REPOSITORY_NAME,
+      owner: process.env.GITHUB_REPOSITORY_OWNER,
+      sha: process.env.COMMIT_SHA,
+      target: process.env.TARGET_SHA,
+      trigger: process.env.GITHUB_BASE_REF ? 'pull' : 'push',
+      branch: process.env.BRANCH_NAME,
+    } as PublishGraphOptions; // NOTE: this options object will vary depending on the CI/CD provider you're using
+    const graphPublisher = new GraphPublisher(app);
+    await graphPublisher.publish(publishOptions);
 
-  //   await app.close();
-  // } else {
-  await app.listen(3000);
-  // }
+    await app.close();
+  } else {
+    await app.listen(3000);
+  }
 }
 bootstrap();
